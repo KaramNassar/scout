2e62b553d7123f0c8907292f41c949ec2033c00d
Karam <karam.nassar@hotmail.com>
2024-06-27 17:12:17 +0300

    Make Filament sidebar collapsible on desktop

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
 			->path('admin')
 			->profile()
 			->login()
+			->sidebarCollapsibleOnDesktop()
 			->colors([
 				'primary' => Color::Amber,
 			])
