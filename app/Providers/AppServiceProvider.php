52c4e35fe96973cdbaa19cf1592a0a84381740eb
Karam <karam.nassar@hotmail.com>
2024-07-05 08:54:38 +0300

    Add a language switcher to the admin panel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,10 @@
 namespace App\Providers;
 
 use App\Policies\ExceptionPolicy;
+use BezhanSalleh\FilamentExceptions\Models\Exception;
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-use BezhanSalleh\FilamentExceptions\Models\Exception;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-	    Gate::policy(Exception::class, ExceptionPolicy::class);
+        Gate::policy(Exception::class, ExceptionPolicy::class);
+
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(config('locales.names'))
+                ->flags(config('locales.flags')); // also accepts a closure
+        });
     }
 }
---
+++ config/locales.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'names' => [
+        'en',
+        'ar',
+        'fr',
+    ],
+
+    'flags' => [
+        'en' => config('app.url') . '/assets/flags/1x1/us.svg',
+        'ar' => config('app.url') . '/assets/flags/1x1/sy.svg',
+        'fr' => config('app.url') . '/assets/flags/1x1/fr.svg',
+    ]
+];
