1c753531fb2af2b882040fb57230eb772d823be0
Karam <karam.nassar@hotmail.com>
2024-07-05 08:17:11 +0300

    Fix badge colors for admin roles

--- app/Filament/Resources/AdminResource.php
+++ app/Filament/Resources/AdminResource.php
@@ -32,148 +32,150 @@ use function filled;
 
 class AdminResource extends Resource
 {
-	protected static ?string $model = Admin::class;
+    protected static ?string $model = Admin::class;
 
-	protected static ?string $navigationIcon = 'heroicon-o-user-plus';
+    protected static ?string $navigationIcon = 'heroicon-o-user-plus';
 
-	protected static ?string $navigationGroup = 'User Management';
+    protected static ?string $navigationGroup = 'User Management';
 
-	public static function form(Form $form): Form
-	{
-		return $form
-			->schema([
-				Section::make()
-					->schema([
-						Grid::make()
-							->schema([
-								SpatieMediaLibraryFileUpload::make('media')
-									->hiddenLabel()
-									->avatar()
-									->collection('avatars')
-									->alignCenter()
-									->columnSpanFull(),
-								TextInput::make('username')
-									->required()
-									->maxLength(255),
-								TextInput::make('email')
-									->email()
-									->required()
-									->maxLength(255),
-								TextInput::make('firstname')
-									->required()
-									->maxLength(255),
-								TextInput::make('lastname')
-									->required()
-									->maxLength(255),
-							]),
-					])
-					->columnSpan([
-						'sm' => 1,
-						'lg' => 2
-					]),
-				Group::make()
-					->schema([
-						Section::make('Role')
-							->schema([
-								Select::make('roles')->label('Role')
-									->hiddenLabel()
-									->relationship('roles', 'name')
-									->getOptionLabelFromRecordUsing(fn(Model $record) => Str::headline($record->name))
-									->multiple()
-									->preload()
-									->maxItems(1)
-									->native(false),
-							])
-							->compact(),
-						Section::make()
-							->schema([
-								TextInput::make('password')
-									->password()
-									->dehydrateStateUsing(fn(string $state): string => Hash::make($state))
-									->dehydrated(fn(?string $state): bool => filled($state))
-									->revealable()
-									->required(),
-								TextInput::make('passwordConfirmation')
-									->password()
-									->dehydrateStateUsing(fn(string $state): string => Hash::make($state))
-									->dehydrated(fn(?string $state): bool => filled($state))
-									->revealable()
-									->same('password')
-									->required(),
-							])
-							->compact()
-							->hidden(fn(string $operation): bool => $operation === 'edit'),
-						Section::make()
-							->schema([
-								Placeholder::make('created_at')
-									->label(__('resource.general.created_at'))
-									->content(fn(Admin $record): ?string => $record->created_at?->diffForHumans()),
-								Placeholder::make('updated_at')
-									->label(__('resource.general.updated_at'))
-									->content(fn(Admin $record): ?string => $record->updated_at?->diffForHumans()),
-							])
-							->hidden(fn(string $operation): bool => $operation === 'create'),
-					])
-					->columnSpan(1),
-			])
-			->columns(3);
-	}
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()
+                    ->schema([
+                        Grid::make()
+                            ->schema([
+                                SpatieMediaLibraryFileUpload::make('media')
+                                    ->hiddenLabel()
+                                    ->avatar()
+                                    ->collection('avatars')
+                                    ->alignCenter()
+                                    ->columnSpanFull(),
+                                TextInput::make('username')
+                                    ->required()
+                                    ->maxLength(255),
+                                TextInput::make('email')
+                                    ->email()
+                                    ->required()
+                                    ->maxLength(255),
+                                TextInput::make('firstname')
+                                    ->required()
+                                    ->maxLength(255),
+                                TextInput::make('lastname')
+                                    ->required()
+                                    ->maxLength(255),
+                            ]),
+                    ])
+                    ->columnSpan([
+                        'sm' => 1,
+                        'lg' => 2
+                    ]),
+                Group::make()
+                    ->schema([
+                        Section::make('Role')
+                            ->schema([
+                                Select::make('roles')->label('Role')
+                                    ->hiddenLabel()
+                                    ->relationship('roles', 'name')
+                                    ->getOptionLabelFromRecordUsing(fn(Model $record) => Str::headline($record->name))
+                                    ->multiple()
+                                    ->preload()
+                                    ->maxItems(1)
+                                    ->native(false),
+                            ])
+                            ->compact(),
+                        Section::make()
+                            ->schema([
+                                TextInput::make('password')
+                                    ->password()
+                                    ->dehydrateStateUsing(fn(string $state): string => Hash::make($state))
+                                    ->dehydrated(fn(?string $state): bool => filled($state))
+                                    ->revealable()
+                                    ->required(),
+                                TextInput::make('passwordConfirmation')
+                                    ->password()
+                                    ->dehydrateStateUsing(fn(string $state): string => Hash::make($state))
+                                    ->dehydrated(fn(?string $state): bool => filled($state))
+                                    ->revealable()
+                                    ->same('password')
+                                    ->required(),
+                            ])
+                            ->compact()
+                            ->hidden(fn(string $operation): bool => $operation === 'edit'),
+                        Section::make()
+                            ->schema([
+                                Placeholder::make('created_at')
+                                    ->label(__('resource.general.created_at'))
+                                    ->content(fn(Admin $record): ?string => $record->created_at?->diffForHumans()),
+                                Placeholder::make('updated_at')
+                                    ->label(__('resource.general.updated_at'))
+                                    ->content(fn(Admin $record): ?string => $record->updated_at?->diffForHumans()),
+                            ])
+                            ->hidden(fn(string $operation): bool => $operation === 'create'),
+                    ])
+                    ->columnSpan(1),
+            ])
+            ->columns(3);
+    }
 
-	public static function table(Table $table): Table
-	{
-		return $table
-			->columns([
-				SpatieMediaLibraryImageColumn::make('media')->label('Avatar')
-					->collection('avatars')
-					->wrap(),
-				TextColumn::make('username')->label('Username')
-					->description(fn(Model $record) => $record->firstname.' '.$record->lastname)
-					->searchable(),
-				TextColumn::make('roles.name')->label('Role')
-					->formatStateUsing(fn($state): string => Str::headline($state))
-					->badge()
-					->color(fn(string $state): string => match ($state) {
-						'super_admin' => 'success',
-						'admin' => 'warning',
-						'editor' => 'info',
-						'default' => 'gray'
-					}),
-				TextColumn::make('email')
-					->searchable(),
-				TextColumn::make('created_at')
-					->dateTime()
-					->sortable()
-					->toggleable(isToggledHiddenByDefault: true),
-				TextColumn::make('updated_at')
-					->dateTime()
-					->sortable()
-					->toggleable(isToggledHiddenByDefault: true),
-			])
-			->filters([
-				TrashedFilter::make(),
-			])
-			->actions([
-				EditAction::make(),
-				DeleteAction::make(),
-				RestoreAction::make(),
-				ForceDeleteAction::make(),
-			]);
-	}
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                SpatieMediaLibraryImageColumn::make('media')->label('Avatar')
+                    ->collection('avatars')
+                    ->wrap(),
+                TextColumn::make('username')->label('Username')
+                    ->description(fn(Model $record) => $record->firstname.' '.$record->lastname)
+                    ->searchable(),
+                TextColumn::make('roles.name')->label('Role')
+                    ->formatStateUsing(fn($state): string => Str::headline($state))
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        'super_admin' => 'success',
+                        'admin' => 'warning',
+                        'moderator' => 'danger',
+                        'developer' => 'primary',
+                        'editor' => 'info',
+                        default => 'gray'
+                    }),
+                TextColumn::make('email')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->actions([
+                EditAction::make(),
+                DeleteAction::make(),
+                RestoreAction::make(),
+                ForceDeleteAction::make(),
+            ]);
+    }
 
-	public static function getRelations(): array
-	{
-		return [
-			//
-		];
-	}
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
 
-	public static function getPages(): array
-	{
-		return [
-			'index'  => ListAdmins::route('/'),
-			'create' => CreateAdmin::route('/create'),
-			'edit'   => EditAdmin::route('/{record}/edit'),
-		];
-	}
+    public static function getPages(): array
+    {
+        return [
+            'index'  => ListAdmins::route('/'),
+            'create' => CreateAdmin::route('/create'),
+            'edit'   => EditAdmin::route('/{record}/edit'),
+        ];
+    }
 
 }
