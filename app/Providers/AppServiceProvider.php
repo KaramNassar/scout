9f64b0aa5c657c772a39ea2ce1d46a72e7603a9c
Karam <karam.nassar@hotmail.com>
2024-07-08 10:50:31 +0300

    Remove Language switch package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use App\Policies\ActivityPolicy;
 use App\Policies\ExceptionPolicy;
 use BezhanSalleh\FilamentExceptions\Models\Exception;
-use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Activitylog\Models\Activity;
@@ -28,10 +27,5 @@ class AppServiceProvider extends ServiceProvider
         Gate::policy(Exception::class, ExceptionPolicy::class);
         Gate::policy(Activity::class, ActivityPolicy::class);
 
-        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
-            $switch
-                ->locales(config('locales.names'))
-                ->flags(config('locales.flags')); // also accepts a closure
-        });
     }
 }
