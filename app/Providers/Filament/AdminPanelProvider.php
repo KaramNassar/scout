c194795a32826fb46ef716353ba09b938bf658f0
Karam <karam.nassar@hotmail.com>
2024-07-10 07:52:35 +0300

    Add Backup package

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Backups;
 use App\Filament\Pages\HealthCheckResults;
-use App\Filament\Resources\ActivityLogResource;
 use App\Livewire\CustomProfile;
 use BezhanSalleh\FilamentExceptions\FilamentExceptionsPlugin;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
@@ -26,6 +26,7 @@ use Jeffgreco13\FilamentBreezy\BreezyCore;
 use Joaopaulolndev\FilamentGeneralSettings\FilamentGeneralSettingsPlugin;
 use Joaopaulolndev\FilamentGeneralSettings\Models\GeneralSetting;
 use Rmsramos\Activitylog\ActivitylogPlugin;
+use ShuvroRoy\FilamentSpatieLaravelBackup\FilamentSpatieLaravelBackupPlugin;
 use ShuvroRoy\FilamentSpatieLaravelHealth\FilamentSpatieLaravelHealthPlugin;
 use Tapp\FilamentMailLog\FilamentMailLogPlugin;
 use TomatoPHP\FilamentTranslations\FilamentTranslationsPlugin;
@@ -33,65 +34,67 @@ use TomatoPHP\FilamentTranslations\FilamentTranslationsSwitcherPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
-	public function panel(Panel $panel): Panel
-	{
-		return $panel
-			->default()
-			->id('admin')
-			->path('admin')
-			->login()
-			->sidebarCollapsibleOnDesktop()
-			->colors([
-				'primary' => Color::Amber,
-				'gray' => 'rgb(107, 114, 128)',
-			])
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->default()
+            ->id('admin')
+            ->path('admin')
+            ->login()
+            ->sidebarCollapsibleOnDesktop()
+            ->colors([
+                'primary' => Color::Amber,
+                'gray'    => 'rgb(107, 114, 128)',
+            ])
             ->favicon(GeneralSetting::value('site_favicon'))
-			->authGuard('admin')
-			->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
-			->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-			->pages([
-				Pages\Dashboard::class,
-			])
-			->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-			->widgets([
-				Widgets\AccountWidget::class,
-				Widgets\FilamentInfoWidget::class,
-			])
-			->middleware([
-				EncryptCookies::class,
-				AddQueuedCookiesToResponse::class,
-				StartSession::class,
-				AuthenticateSession::class,
-				ShareErrorsFromSession::class,
-				VerifyCsrfToken::class,
-				SubstituteBindings::class,
-				DisableBladeIconComponents::class,
-				DispatchServingFilamentEvent::class,
-			])
-			->authMiddleware([
-				Authenticate::class,
-			])
-			->plugins([
-				FilamentShieldPlugin::make(),
-				BreezyCore::make()
-					->myProfile()
-					->myProfileComponents([
-						'personal_info' => CustomProfile::class
-					]),
-				FilamentGeneralSettingsPlugin::make()
-					->canAccess(fn() => auth('admin')->user()->can('page_GeneralSettingsPage'))
-					->setSort(3)
-					->setIcon('heroicon-o-cog')
-					->setNavigationGroup('Settings')
-					->setTitle('General Settings')
-					->setNavigationLabel('General Settings'),
-				FilamentExceptionsPlugin::make(),
+            ->authGuard('admin')
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->widgets([
+                Widgets\AccountWidget::class,
+                Widgets\FilamentInfoWidget::class,
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ])
+            ->plugins([
+                FilamentShieldPlugin::make(),
+                BreezyCore::make()
+                    ->myProfile()
+                    ->myProfileComponents([
+                        'personal_info' => CustomProfile::class
+                    ]),
+                FilamentGeneralSettingsPlugin::make()
+                    ->canAccess(fn() => auth('admin')->user()->can('page_GeneralSettingsPage'))
+                    ->setSort(3)
+                    ->setIcon('heroicon-o-cog')
+                    ->setNavigationGroup('Settings')
+                    ->setTitle('General Settings')
+                    ->setNavigationLabel('General Settings'),
+                FilamentExceptionsPlugin::make(),
                 ActivitylogPlugin::make(),
                 FilamentMailLogPlugin::make(),
                 FilamentTranslationsPlugin::make(),
                 FilamentTranslationsSwitcherPlugin::make(),
                 FilamentSpatieLaravelHealthPlugin::make()
                     ->usingPage(HealthCheckResults::class),
-			]);
-	}
+                FilamentSpatieLaravelBackupPlugin::make()
+                    ->usingPage(Backups::class),
+            ]);
+    }
 }
