8db8c211d6299bdaa163fbcd3fe1526caded749f
Karam <karam.nassar@hotmail.com>
2024-07-17 11:46:40 +0300

    Fix Activities section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,35 +68,35 @@
     <x-content-showcase :heading="__('Activities')">
         <div class="w-full h-full">
             <div class="max-w-6xl mx-auto">
-                <ul class="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+                <ul class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5">
 
                     <li>
-                        <x-card.gallary title="How we design and code open-source projects?"
+                        <x-card.gallery title="How we design and code open-source projects?"
                                         :src="asset('storage/images/7.jpg')"/>
                     </li>
 
                     <li>
-                        <x-card.gallary title="How we design and code open-source projects?"
+                        <x-card.gallery title="How we design and code open-source projects?"
                                         :src="asset('storage/images/5.jpg')"/>
                     </li>
 
                     <li>
-                        <x-card.gallary title="How we design and code open-source projects?"
+                        <x-card.gallery title="How we design and code open-source projects?"
                                         :src="asset('storage/images/3.jpg')"/>
                     </li>
 
                     <li>
-                        <x-card.gallary title="How we design and code open-source projects?"
+                        <x-card.gallery title="How we design and code open-source projects?"
                                         :src="asset('storage/images/4.jpg')"/>
                     </li>
 
                     <li>
-                        <x-card.gallary title="How we design and code open-source projects?"
+                        <x-card.gallery title="How we design and code open-source projects?"
                                         :src="asset('storage/images/2.jpg')"/>
                     </li>
 
                     <li>
-                        <x-card.gallary title="How we design and code open-source projects?"
+                        <x-card.gallery title="How we design and code open-source projects?"
                                         :src="asset('storage/images/1.jpg')"/>
                     </li>
 
