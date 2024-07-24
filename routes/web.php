a6fd59f39557461daadeaba5a8ed747bd9c85da3
Karam <karam.nassar@hotmail.com>
2024-07-24 08:24:21 +0300

    Delete Auth files

--- routes/web.php
+++ routes/web.php
@@ -1,59 +1,19 @@
 <?php
 
-use App\Http\Controllers\Auth\EmailVerificationController;
-use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\LocalesController;
 use App\Http\Controllers\SearchResultsController;
-use App\Livewire\Auth\Login;
-use App\Livewire\Auth\Passwords\Confirm;
-use App\Livewire\Auth\Passwords\Email;
-use App\Livewire\Auth\Passwords\Reset;
-use App\Livewire\Auth\Register;
-use App\Livewire\Auth\Verify;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
-Route::view('/troops', 'troops.index')->name('troops.index');
-Route::view('/troops/{troop:slug}', 'troops.show')->name('troops.show');
 Route::view('/about-the-scout', 'about')->name('about');
 Route::view('/history-of-the-scout', 'history')->name('history-of-the-scout');
 Route::view('/contact', 'contact')->name('contact');
+
+Route::view('/troops', 'troops.index')->name('troops.index');
+Route::view('/troops/{troop:slug}', 'troops.show')->name('troops.show');
 Route::view('/all-news', 'all-news')->name('all-news');
 
 Route::get('/search-results', SearchResultsController::class)->name('search-results');
 
 Route::get('/locale/{locale}', LocalesController::class)->name('locale.switch');
-
-
-//Route::middleware('guest')->group(function () {
-//    Route::get('login', Login::class)
-//        ->name('login');
-//
-//    Route::get('register', Register::class)
-//        ->name('register');
-//});
-//
-//Route::get('password/reset', Email::class)
-//    ->name('password.request');
-//
-//Route::get('password/reset/{token}', Reset::class)
-//    ->name('password.reset');
-//
-//Route::middleware('auth')->group(function () {
-//    Route::get('email/verify', Verify::class)
-//        ->middleware('throttle:6,1')
-//        ->name('verification.notice');
-//
-//    Route::get('password/confirm', Confirm::class)
-//        ->name('password.confirm');
-//});
-//
-//Route::middleware('auth')->group(function () {
-//    Route::get('email/verify/{id}/{hash}', EmailVerificationController::class)
-//        ->middleware('signed')
-//        ->name('verification.verify');
-//
-//    Route::post('logout', LogoutController::class)
-//        ->name('logout');
-//});
