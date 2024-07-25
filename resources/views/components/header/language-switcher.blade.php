aaeb93161ef5aa38c42d1733124755909d04425a
Karam <karam.nassar@hotmail.com>
2024-07-25 11:10:56 +0300

    Fix Language Switcher

--- resources/views/components/header/language-switcher.blade.php
+++ resources/views/components/header/language-switcher.blade.php
@@ -9,12 +9,16 @@
          class="absolute end-0 mt-2 w-40 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 focus:outline-none">
         <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
             @foreach(config('locales') as $locale => $data)
-                <a href="{{ route('locale.switch', $locale) }}"
-                   class="flex gap-2 items-center px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 w-full"
-                   role="menuitem">
-                    <img src="https://flagcdn.com/{{ $data['flag'] }}.svg" alt="Flag" class="h-5 w-5 mr-2">
-                    {{ $data['display'] }}
-                </a>
+                @if(config('app.locale') != $locale)
+                    <a href="{{ route('locale.switch', $locale) }}"
+                       class="flex gap-2 items-center flex-row-reverse  px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 w-full"
+                       role="menuitem">
+                        <img src="https://flagcdn.com/{{ $data['flag'] }}.svg" alt="Flag" class="h-5 w-5">
+                        <span class="font-medium">
+                            {{ $data['display'] }}
+                        </span>
+                    </a>
+                @endif
             @endforeach
 
         </div>
