ed3096302b13990bcb4a47f6d1285df51df5cd30
Karam <karam.nassar@hotmail.com>
2024-08-08 11:51:38 +0300

    Fix image placeholder

--- resources/views/components/image.blade.php
+++ resources/views/components/image.blade.php
@@ -1,13 +1,27 @@
 @props(['model'])
 
-@if ($model->featuredImage?->hasCuration('thumbnail'))
-    <x-curator-curation :media="$model->featuredImage" curation="thumbnail" {{ $attributes }}/>
-@else
-    <x-curator-glider
-        {{ $attributes->class(['object-cover']) }}
-        :media="$model->featuredImage"
-        :width="600"
-        :height="450"
-        format="webp"
-    />
-@endif
+<div x-data="{ imageLoaded: false }" class="relative overflow-hidden w-full">
+    <div role="status"
+         x-show="!imageLoaded"
+        {{ $attributes->class(['flex justify-center items-center bg-gray-300 dark:bg-gray-500']) }}>
+        <x-image-placeholder/>
+    </div>
+
+    @if ($model->featuredImage?->hasCuration('thumbnail'))
+        <x-curator-curation
+            :media="$model->featuredImage"
+            curation="thumbnail"
+            {{ $attributes }}
+            x-init="imageLoaded = true"/>
+    @else
+        <x-curator-glider
+            {{ $attributes->class(['object-cover']) }}
+            :media="$model->featuredImage"
+            :width="600"
+            :height="450"
+            format="webp"
+            x-init="imageLoaded = true"
+        />
+    @endif
+
+</div>
