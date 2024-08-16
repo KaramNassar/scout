70cbf373a9b3f0a0c511749bccc7083fdf795002
Karam <karam.nassar@hotmail.com>
2024-08-16 11:13:27 +0300

    Change general settings

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ app()->getLocale() === 'ar' ? 'rtl' : 'ltr' }}" x-data="{ openSearch: false, showMapModal: false }" x-bind:class="{ 'overflow-hidden': openSearch ||  showMapModal }" x-cloak>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ app()->getLocale() === 'ar' ? 'rtl' : 'ltr' }}"
+      x-data="{ openSearch: false, showMapModal: false }"
+      x-bind:class="{ 'overflow-hidden': openSearch ||  showMapModal }" x-cloak>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,11 +21,16 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {!! $settings->more_configs['header_code'] !!}
 </head>
 
 <body class="dark:bg-gray-800 font-roboto rtl:font-cairo">
 @yield('body')
 
 @stack('scripts')
+
+{!! $settings->more_configs['footer_code'] !!}
+
 </body>
 </html>
